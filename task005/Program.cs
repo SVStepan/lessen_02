01d2d875d198ef7de25930a4c45c8a01275eb291
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-03 19:04:50 +0300

    условие

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите имя пользователя:");
+string user = Console.ReadLine();
+// if(user == "Степан")
+if(user.ToLower() == "степан") // .ToLower() позволяет все символы в строке перевести в нижний регистр
+{
+    Console.WriteLine("Ура! Привет Степан");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(user);
+}
