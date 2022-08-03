595bce7e92a6a9f6c5fdf669a91ed855a35857a5
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-03 18:34:12 +0300

    div

---
+++ task004_Div/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+int numberF = 12;
+int numberD = 5;
+Console.WriteLine(numberF / numberD);
+
+double numberC = 12;
+double numberV = 5;
+Console.WriteLine(numberC / numberV);
