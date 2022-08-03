61234b8e33ebdeb4994c07d1507c3b40566663d3
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-03 18:46:51 +0300

    sum-1

--- task003/Program.cs
+++ task003/Program.cs
@@ -1,8 +1,14 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
 
 int numberF = 30;
 int numberD = 54;
 int res = numberF + numberD;
 Console.WriteLine(res);
+
+int numberA = new Random().Next(1, 10); // случайное число из 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
