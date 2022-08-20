a2c42f3f7478228099e919e7e9eefa92c5cf2fb4
Stepan Sm <stepan_smirnoff@mail.ru>
2022-08-20 14:58:15 +0300

    лекция 4

--- task009_method/Program.cs
+++ task009_method/Program.cs
@@ -1,4 +1,132 @@
-﻿// int[] array = { 12, 21, 53, 84, 91, 75, 26, 17, 28, 91 };
+﻿Console.Clear();
+
+// // Вид 1 метод - ничего не принимают, ничего не возвращают
+// void Method1()
+// {
+//     Console.WriteLine("Автор Степан");
+// }
+// Method1(); // вариант как вызывается как этот метод
+
+
+// // ВИД 2  что-то принимают, но ничего не возвращают
+// void Method2(string msg)  // принимает какие-то аргументы
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "текст сообщения");
+
+// void Method21(string msg, int count) // принимает какие-то несколько аргументов
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21("Текст", 4);
+// Method21(msg: "Текст", count: 4);  // явно указываем наименование аргументов
+// Method21(count: 4, msg: "новый текст"); // когда аргументы именнованные,то не обязвтельно их писать по порядку
+
+// // Вид 3  ничего не принимают, но что-то возвращают
+
+// int Method3()  // если что-то возвращает, то обязательно должны указать тип данных, которые мы ожидаем.
+// {
+//     return DateTime.Now.Year;
+// }
+// int Year = Method3();
+// Console.WriteLine(Year);
+
+
+// // Вид 4 Что-то принимают и что-то возвращают
+
+// string Method4(int count, string Text)
+// {
+//     int i = 0;
+//     string result = String.Empty;  // пустая строка
+
+//     while (i < count)
+//     {
+//         result = result + Text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "as_df");
+// Console.WriteLine(res);
+
+
+
+// цикл for
+
+// string Method4(int count, string Text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + Text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "as_df,");
+// Console.WriteLine(res);
+
+
+// цикл в цикле
+// вывод таблицы умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");  // интерполяция строк
+//     }
+//     Console.WriteLine(); // переход на новую строку
+// }
+
+
+// ==== Работа с текстом
+//дан текст, в тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить на маленькие "с".
+
+// Яснали задача?
+
+string text = "-Я думаю, - сказал князь, улыбаясь, -что, "
+            + "ежели бы вас послали вместо нашегомилого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             01234
+// s[3] // r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i< length; i++)
+    {
+        if(text[i] == oldValue) result = result+ $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText, 'В', 'в');
+Console.WriteLine(newText);
+
+
+
+
+// int[] array = { 12, 21, 53, 84, 91, 75, 26, 17, 28, 91 };
 
 // int n = array.Length;
 // int find = 91;
---
+++ task011_Rekuesia/Program.cs
@@ -0,0 +1,37 @@
+﻿// Реккурсия
+//Факториал 5! = 5*4*3*2*1
+// 5! = 5*4!
+//    4! = 4*3!
+
+// int Factorial(int n)
+// double Factorial(int n)  // Проблему переполнения решаем int меняем на double
+// {
+//     // 1! = 1  и 0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// } 
+//     //  Console.WriteLine(Factorial(3)); // 3! = 1*2*3 = 6
+//     //  чем больше значение n тем вероятнее наличие проблем (переполнение, сложность вычисление в плане медлительность)
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// Фибоначи 
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+
+// double fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;  // || - или
+//     else return fibonacci(n-1) + fibonacci(n-2);  // return возвращаем значение
+// }
+
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {fibonacci(i)}");
+// }
